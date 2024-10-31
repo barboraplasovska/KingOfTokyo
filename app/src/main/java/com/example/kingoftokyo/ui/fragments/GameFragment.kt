039e8524559405b0f6e08d1070a630f56d99069a
Valentin Lilian <lilian@dynseo.com>
2024-10-31 12:14:02 +0100

    feat(fragments): re-added

--- app/src/main/java/com/example/kingoftokyo/ui/fragments/GameFragment.kt
+++ app/src/main/java/com/example/kingoftokyo/ui/fragments/GameFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DiceRollFragment.newInstance] factory method to
+ * Use the [GameFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DiceRollFragment : Fragment() {
+class GameFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class DiceRollFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dice_roll, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class DiceRollFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DiceRollFragment.
+         * @return A new instance of fragment GameFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DiceRollFragment().apply {
+            GameFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
