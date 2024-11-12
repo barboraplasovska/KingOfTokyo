8d3cc9b4ee3118eb3c4896f7e49fec12e57c6d4b
Bianca Onciul <bianca.onciul@epita.fr>
2024-11-12 15:15:25 +0100

    game doesn't crash when selecting current player

--- app/src/main/java/com/example/kingoftokyo/ui/fragments/GameFragment.kt
+++ app/src/main/java/com/example/kingoftokyo/ui/fragments/GameFragment.kt
@@ -1,5 +1,7 @@
 package com.example.kingoftokyo.ui.fragments
 
+import PlayerModel
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -7,13 +9,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.kingoftokyo.R
 import com.example.kingoftokyo.core.viewModels.MainViewModel
 import com.example.kingoftokyo.core.services.GameService
@@ -28,8 +33,10 @@ class GameFragment : Fragment() {
     private lateinit var dragon: FragmentContainerView
     private lateinit var lizard: FragmentContainerView
     private lateinit var robot: FragmentContainerView
+    private lateinit var monsterCards: List<FragmentContainerView>
     private lateinit var diceFragment: DiceFragment
     private lateinit var finishTurnButton: Button
+    private var selectedMonster: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,13 +51,22 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Initialize game with selected monster index
-        val selectedMonster = arguments?.getInt("selectedMonster") ?: 0
+        selectedMonster = arguments?.getInt("selectedMonster") ?: 0
         mainViewModel.startGame(selectedMonster)
 
         diceFragment = DiceFragment()
         childFragmentManager.beginTransaction()
             .replace(R.id.diceFragmentContainer, diceFragment)
             .commitNow()
+
+        demon = view.findViewById(R.id.demonCard)
+        dragon = view.findViewById(R.id.dragonCard)
+        lizard = view.findViewById(R.id.lizardCard)
+        robot = view.findViewById(R.id.robotCard)
+        monsterCards = listOf(demon, dragon, lizard, robot)
+        finishTurnButton = view.findViewById(R.id.finishTurnButton)
+
+        setBackgroundMonster(monsterCards[selectedMonster], R.drawable.monster_card_selected_background)
 
         // Observe changes in player data and update the UI accordingly
         mainViewModel.players.observe(viewLifecycleOwner) { players ->
@@ -65,28 +81,18 @@ class GameFragment : Fragment() {
             (childFragmentManager.findFragmentById(R.id.robotCard) as? MonsterCardFragment)?.setMonsterData(robotPlayer)
             (childFragmentManager.findFragmentById(R.id.lizardCard) as? MonsterCardFragment)?.setMonsterData(lizardPlayer)
 
-            var currentPlayer = mainViewModel.getCurrentPlayer()
-            if (currentPlayer.playerType == PlayerType.BOT) {
+        }
+
+        mainViewModel.currentPlayer.observe(viewLifecycleOwner) { currentPlayer ->
+            updateBg(currentPlayer)
+            if (currentPlayer?.playerType == PlayerType.BOT) {
                 diceFragment.setBotTurn()
+                Thread.sleep(4000)
             } else {
                 diceFragment.setPlayerTurn()
             }
-
         }
 
-
-        demon = view.findViewById(R.id.demonCard)
-        dragon = view.findViewById(R.id.dragonCard)
-        lizard = view.findViewById(R.id.lizardCard)
-        robot = view.findViewById(R.id.robotCard)
-        finishTurnButton = view.findViewById(R.id.finishTurnButton)
-
-        when (selectedMonster) {
-            0 -> setBackgroundMonster(demon, R.drawable.monster_player_selected)
-            1 -> setBackgroundMonster(dragon, R.drawable.monster_player_selected)
-            2 -> setBackgroundMonster(lizard, R.drawable.monster_player_selected)
-            3 -> setBackgroundMonster(robot, R.drawable.monster_player_selected)
-        }
 
         finishTurnButton.setOnClickListener {
             botTurn()
@@ -99,7 +105,60 @@ class GameFragment : Fragment() {
         diceFragment.setBotTurn()
     }
 
+    private fun updateBg(currentPlayer: PlayerModel?) {
+        if (currentPlayer == null)
+            return
+        if (currentPlayer.playerType == PlayerType.HUMAN) {
+            setBackgroundMonster(monsterCards[selectedMonster], R.drawable.monster_player_selected)
+            for (i in monsterCards.indices) {
+                if (i == selectedMonster) {
+                    continue
+                }
+                setBackgroundMonster(monsterCards[i], R.drawable.monster_card_background)
+            }
+        }
+        else {
+            setBackgroundMonster(monsterCards[selectedMonster], R.drawable.monster_current_player_background)
+            for (i in monsterCards.indices) {
+                if (i == selectedMonster) {
+                    continue
+                }
+                setBackgroundMonster(monsterCards[i], R.drawable.monster_card_background)
+            }
+            when (currentPlayer.monsterName) {
+                "Demon" -> setBackgroundMonster(demon, R.drawable.monster_card_background)
+                "Dragon" -> setBackgroundMonster(dragon, R.drawable.monster_card_background)
+                "Lizard" -> setBackgroundMonster(lizard, R.drawable.monster_card_background)
+                "Robot" -> setBackgroundMonster(robot, R.drawable.monster_card_background)
+            }
+        }
+    }
+
     private fun setBackgroundMonster(card: FragmentContainerView, drawable: Int) {
         card.setBackgroundResource(drawable)
+    }
+
+    private fun showGameOverModal(player: PlayerModel) {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.game_over_modal)
+
+        val titleTextView = dialog.findViewById<TextView>(R.id.modalTitle)
+        titleTextView.text = "${player.monsterName} is the King of Tokyo"
+
+        var monsterImage: ImageView = dialog.findViewById(R.id.monsterImage)
+        when (player.monsterName) {
+            "Demon" -> monsterImage.setImageResource(R.drawable.demon)
+            "Dragon" -> monsterImage.setImageResource(R.drawable.dragon)
+            "Lizard" -> monsterImage.setImageResource(R.drawable.lizard)
+            "Robot" -> monsterImage.setImageResource(R.drawable.robot)
+        }
+
+        val dismissButton = dialog.findViewById<Button>(R.id.dismissButton)
+        dismissButton.setOnClickListener {
+            dialog.dismiss()
+            findNavController().navigate(R.id.action_gameFragment_to_welcomeFragment)
+        }
+
+        dialog.show()
     }
 }
